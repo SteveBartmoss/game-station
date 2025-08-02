d78ce22d10d805c59a9cf1f308a530f0c1e7a0b3
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-01 21:24:35 -0600

    feat: Se implemento una pagina para rutas por defecto

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import './css/notfound.css'
+
+export function NotFound(){
+    return(
+        <>
+            <div className="div-main">
+                <h1>404</h1>
+                <h3>Parace que lo que buscas esta en otro castillo</h3>
+            </div>
+        </>
+    )
+}
