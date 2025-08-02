0e1356a31aa1eb21697d9d05cf18d320dc5c60c6
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-01 21:31:29 -0600

    feat: Se implementaron elementos para la barra de navegacion

--- src/components/navbar/Navabar.jsx
+++ src/components/navbar/Navabar.jsx
@@ -1,9 +1,23 @@
+import './navbar.css'
+import { NavLink } from "react-router-dom";
 
 
 export function NavBar(){
     return(
         <>
-            <h1>NavBar</h1>
+            <div className='div-nav-bar'>
+                <ul className="nav-ul">
+                    <li>
+                        <NavLink className={"nav-item"} to={"/"}>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={"nav-item"} to={"/about"}>About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={"nav-item"} to={"/steve"}>Steve</NavLink>
+                    </li>
+                </ul>
+            </div>
         </>
     )
 }
